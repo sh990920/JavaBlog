2147797168602210d93e753ef41165b180be072a
test1 <test1@sh0920>
2023-05-16 01:54:22 +0900

    example

---
+++ test1/src/test1/Test1.java
@@ -0,0 +1,12 @@
+package test1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!3");
+		System.out.println("실행은 ctrl + F11");
+		System.out.println("화면 글자 크기 변경은  ctrl + '+' 혹은 '-' 로 사용한다.");
+	}
+
+}
